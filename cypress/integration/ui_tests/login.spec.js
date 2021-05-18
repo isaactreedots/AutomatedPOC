ab482a76fac0c1d2198588fe1bf2a5afbcd816ff
DannyDwi <danny@thetreedots.com>
2021-05-18 17:19:28 +0700

    Revisi 1 - ubah path - hapus gitignore - remove token

--- cypress/integration/ui_tests/login.spec.js
+++ cypress/integration/ui_tests/login.spec.js
@@ -1,4 +1,4 @@
-import LoginPage from '../.../../../pages/loginPage';
+import LoginPage from '../../pages/loginPage';
 
 describe('Login Test', () => {
     beforeEach(function () {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,7 +3,7 @@ Cypress.Commands.add('requestGraphQl', operationName => {
         url: '/',
         method: 'POST',
         headers: {
-            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIyMDgsInVzZXJUeXBlSWQiOjYsImJ1eWVyVHlwZSI6MiwiY291bnRyeSI6eyJpZCI6MTkzLCJuYW1lIjoiU0ciLCJkZXNjcmlwdGlvbiI6IlNpbmdhcG9yZSIsImN1cnJlbmN5X2NvZGUiOiJTR0QiLCJjdXJyZW5jeV9zeW1ib2wiOiJTJCIsImFjdGl2ZSI6dHJ1ZX0sImlhdCI6MTYyMTI0MDE5MX0.fgFzpKDjrRkUKko9-JncZxkyDvOKe5KL0Lrh7fF93PI',
+            'Authorization': '',
             'Content-Type': 'application/json'
         },
         body: {
