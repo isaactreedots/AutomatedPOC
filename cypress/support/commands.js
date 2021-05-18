91861f1f250a478ad1950de4365121fd8b9a3de6
isaactreedots <83795864+isaactreedots@users.noreply.github.com>
2021-05-18 14:26:06 +0800

    Update commands.js
    Remove sensitive key

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,7 +3,7 @@ Cypress.Commands.add('requestGraphQl', operationName => {
         url: '/',
         method: 'POST',
         headers: {
-            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIwOTQsInVzZXJUeXBlSWQiOjYsImJ1eWVyVHlwZSI6MSwiY291bnRyeSI6eyJpZCI6MTkzLCJuYW1lIjoiU0ciLCJkZXNjcmlwdGlvbiI6IlNpbmdhcG9yZSIsImN1cnJlbmN5X2NvZGUiOiJTR0QiLCJjdXJyZW5jeV9zeW1ib2wiOiJTJCIsImFjdGl2ZSI6dHJ1ZX0sImlhdCI6MTYyMDE3MzcxMX0.monHX_4UCDFpjXy1slEp4AEIEUfBTCB2wCLdm0LTX8k',
+            'Authorization': '',
             'Content-Type': 'application/json'
         },
         body: {
